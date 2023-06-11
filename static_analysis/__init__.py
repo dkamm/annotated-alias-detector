ca38b012a45417f6fc8ad2a21a5462580b15e293
dkamm <davidfkamm@gmail.com>
2023-06-11 02:20:12 -0700

    Updates to actually visit all imports

--- static_analysis/__init__.py
+++ static_analysis/__init__.py
@@ -1,16 +1,227 @@
 
 import os
 import ast
+import sys
+import pkgutil
 import importlib
 from pathlib import Path
 
 builtin_modules = [
-    "os",
-    "importlib",
-    "typing"
+    'abc',
+    'aifc',
+    'argparse',
+    'array',
+    'ast',
+    'asynchat',
+    'asyncio',
+    'asyncore',
+    'atexit',
+    'audioop',
+    'base64',
+    'bdb',
+    'binascii',
+    'binhex',
+    'bisect',
+    'builtins',
+    'bz2',
+    'cProfile',
+    'calendar',
+    'cgi',
+    'cgitb',
+    'chunk',
+    'cmath',
+    'cmd',
+    'code',
+    'codecs',
+    'codeop',
+    'collections',
+    'colorsys',
+    'compileall',
+    'concurrent',
+    'configparser',
+    'contextlib',
+    'copy',
+    'copyreg',
+    'crypt',
+    'csv',
+    'ctypes',
+    'curses',
+    'datetime',
+    'dbm',
+    'decimal',
+    'difflib',
+    'dis',
+    'distutils',
+    'doctest',
+    'email',
+    'encodings',
+    'enum',
+    'errno',
+    'faulthandler',
+    'fcntl',
+    'filecmp',
+    'fileinput',
+    'fnmatch',
+    'formatter',
+    'fractions',
+    'ftplib',
+    'functools',
+    'gc',
+    'getopt',
+    'getpass',
+    'gettext',
+    'glob',
+    'grp',
+    'gzip',
+    'hashlib',
+    'heapq',
+    'hmac',
+    'html',
+    'http',
+    'imaplib',
+    'imghdr',
+    'imp',
+    'importlib',
+    'inspect',
+    'io',
+    'ipaddress',
+    'itertools',
+    'json',
+    'keyword',
+    'lib2to3',
+    'linecache',
+    'locale',
+    'logging',
+    'lzma',
+    'mailbox',
+    'mailcap',
+    'marshal',
+    'math',
+    'mimetypes',
+    'mmap',
+    'modulefinder',
+    'msilib',
+    'msvcrt',
+    'multiprocessing',
+    'netrc',
+    'nis',
+    'nntplib',
+    'numbers',
+    'operator',
+    'optparse',
+    'os',
+    'ossaudiodev',
+    'parser',
+    'pathlib',
+    'pdb',
+    'pickle',
+    'pickletools',
+    'pipes',
+    'pkgutil',
+    'pkg_resources',
+    'platform',
+    'plistlib',
+    'poplib',
+    'posix',
+    'pprint',
+    'profile',
+    'pstats',
+    'pty',
+    'pwd',
+    'py_compile',
+    'pyclbr',
+    'pydoc',
+    'queue',
+    'quopri',
+    'random',
+    're',
+    'readline',
+    'reprlib',
+    'resource',
+    'rlcompleter',
+    'runpy',
+    'sched',
+    'secrets',
+    'select',
+    'selectors',
+    'setuptools',
+    'shelve',
+    'shlex',
+    'shutil',
+    'signal',
+    'site',
+    'smtpd',
+    'smtplib',
+    'sndhdr',
+    'socket',
+    'socketserver',
+    'spwd',
+    'sqlite3',
+    'ssl',
+    'stat',
+    'statistics',
+    'string',
+    'stringprep',
+    'struct',
+    'subprocess',
+    'sunau',
+    'symbol',
+    'symtable',
+    'sys',
+    'sysconfig',
+    'syslog',
+    'tabnanny',
+    'tarfile',
+    'telnetlib',
+    'tempfile',
+    'termios',
+    'test',
+    'textwrap',
+    'threading',
+    'time',
+    'timeit',
+    'tkinter',
+    'token',
+    'tokenize',
+    'trace',
+    'traceback',
+    'tracemalloc',
+    'tty',
+    'turtle',
+    'turtledemo',
+    'types',
+    'typing',
+    'unicodedata',
+    'unittest',
+    'urllib',
+    'uu',
+    'uuid',
+    'venv',
+    'warnings',
+    'wave',
+    'weakref',
+    'webbrowser',
+    'winreg',
+    'winsound',
+    'wsgiref',
+    'xdrlib',
+    'xml',
+    'xmlrpc',
+    'zipapp',
+    'zipfile',
+    'zipimport',
+    'zlib'
 ]
 
 def get_module_from_path(path: Path, level: int):
+    """Extracts module name from path up to level
+    
+    Examples:
+
+    Suppose path is "module_a/foo/__init__.py"
+    if level is 1, then returns "foo"
+    if level is 2, then returns "module_a.foo"
+    """
     assert level >= 1, "level must be >= 1"
 
     if path.name == '__init__.py':
@@ -26,7 +237,30 @@ def get_module_from_path(path: Path, level: int):
 
     return ".".join(reversed(parts))
 
-def get_path_for_relative_import(cwd: Path, module_name: str, level: int):
+def get_abs_module(current_module: str, module: str, level: int) -> str:
+    assert level >= 1, "level must be >= 1"
+
+    parts = current_module.split(".")
+    if level > 1:
+        parts = parts[:-(level-1)]
+    parts.append(module)
+    return ".".join(parts)
+
+
+def get_path_for_relative_import(cwd: Path, module_name: str, level: int) -> Path:
+    """Returns the path for a relative import given the current directory
+
+    Suppose we are processing the line
+
+    `from .types import FooType` in module_a/__init__.py
+
+    Then we would have
+    cwd = "module_a"
+    module_name = "types"
+    level = 1
+
+    and return "module_a/types.py"
+    """
     assert level >= 1, "level must be >= 1"
 
     base_path = cwd
@@ -49,121 +283,215 @@ def get_path_for_relative_import(cwd: Path, module_name: str, level: int):
     else:
         raise FileNotFoundError(f"Could not find module {module_name} at level {level} from {cwd}")
 
-
-def parse_module(module_path):
-    if os.path.isdir(module_path):
-        module_ast = ast.parse('')
-        for filename in os.listdir(module_path):
-            file_path = os.path.join(module_path, filename)
-            file_ast = parse_module(file_path)
-            module_ast.body.extend(file_ast.body)
-    else:
-        with open(module_path, 'r') as f:
-            module_code = f.read()
-        module_ast = ast.parse(module_code, module_path)
-
-    return module_ast
-
-
 class AnnotatedAliasDetector(ast.NodeVisitor):
-    """Detects all annotated aliases in full module import graph"""
+    """Recursively detects all annotated aliases in full module import graph
+    
+    The detector builds a directed graph where symbol names are nodes and edges are imports.
+
+    Example:
+
+    Suppose we had files like
+
+    module_a/types.py:
+        FooType = ...
+
+    module_a/__init__.py:
+        from .types import FooType
+
+    main.py:
+        from module_a import FooType
+
+    The graph would contain path FooType -> module_a.FooType -> module_a.types.FooType 
+    """
 
     def __init__(self):
         self.current_dir = Path(os.getcwd())
         self.current_module = None
         self.visited_modules = set()
         self.annotated_aliases = set()
-        self.symbol_import_graph = {}
+        self.symbol_def_graph = {}
 
-    def get_origin_name(self, name):
+    def get_root_name(self, name: str) -> str:
+        """Returns the source name of name by walking up the symbol definition graph"""
         while name:
             prev_name = name
-            name = self.symbol_import_graph.get(name)
+            name = self.symbol_def_graph.get(name)
         return prev_name
 
-    def visit_module(self, module, level):
-        """Return the full module name
+    def visit_module(self, module: str, level: int) -> str:
+        """Recursively visits the module to detect Annotated aliases.
 
-        ..foo.bar - module="foo.bar", level=2, self.current_module="module_a"
-        
+        The module is treated as a relative import if level >= 1
+
+        Returns the module's full name which may not be known beforehand because of relative imports
         """
-
+        base_module = module.split(".")[0]
         if level >= 1:
             # relative import
 
-            # do some filesys checking to find the module path
-            module_path = get_path_for_relative_import(
-                cwd=self.current_dir,
-                module_name=module,
-                level=level)
+            # convert rel import into absolute
+            abs_module = get_abs_module(self.current_module, module, level)
 
+            if abs_module in self.visited_modules:
+                # already visited
+                return abs_module
+
+            self.visited_modules.add(abs_module)
+
+            # use importlib to find the module path
+            try:
+                module_spec = importlib.util.find_spec(abs_module)
+            except ModuleNotFoundError:
+                # trying to import a module from nonexistant package like "nonexistant_package.module"
+                return abs_module
+
+            if not module_spec:
+                # could not find module
+                return abs_module
+
+            module_path = Path(module_spec.origin)
+            if not str(module_path).endswith(".py"):
+                # could be a shared object or something
+                return abs_module
             code = module_path.read_text()
+
+            ## do some filesys checking to find the module path
+            #try:
+                #module_path = get_path_for_relative_import(
+                    #cwd=self.current_dir,
+                    #module_name=module,
+                    #level=level)
+            #except FileNotFoundError:
+                ## TODO: This is happening for numpy _multiarray_umath. not sure why yet
+                #return module
+
+            #code = module_path.read_text()
             module_node = compile(code, filename="<string>", mode="exec", flags=ast.PyCF_ONLY_AST)
             old_current_dir = self.current_dir
             old_current_module = self.current_module
-
-            rel_module = get_module_from_path(module_path, level)
-            current_module = f"{self.current_module}.{rel_module}"
-            self.current_module = current_module
+            self.current_module = abs_module
+            self.current_dir = module_path.parent
             self.visit(module_node)
             self.current_dir = old_current_dir
             self.current_module = old_current_module
-            return current_module
+            return abs_module
         else:
             # absolute import
-            if module in builtin_modules:
+
+            # in builtins
+            if base_module in builtin_modules:
+                return module
+
+            # already visited
+            if module in self.visited_modules:
                 return module
 
             # use importlib to find the module path
-            module_path = importlib.util.find_spec(module).origin
-            code = Path(module_path).read_text()
+            try:
+                module_spec = importlib.util.find_spec(module)
+            except ModuleNotFoundError:
+                # trying to import a module from nonexistant package like "nonexistant_package.module"
+                return module
+
+            if not module_spec:
+                # could not find module
+                return module
+
+            module_path = Path(module_spec.origin)
+            if not str(module_path).endswith(".py"):
+                # could be a shared object or something
+                return module
+            code = module_path.read_text()
 
             module_node = compile(code, filename="<string>", mode="exec", flags=ast.PyCF_ONLY_AST)
             old_current_module = self.current_module
             old_current_dir = self.current_dir
-            self.current_dir = Path(module_path).parent
+            self.current_dir = module_path.parent
             self.current_module = module
+            self.visited_modules.add(module)
             self.visit(module_node)
             self.current_dir = old_current_dir
             self.current_module = old_current_module
             return module
 
     def visit_Import(self, node):
-
-        self.visit_module(node.module, node.level)
+        for name in node.names:
+            module = name.name
+            base_module = module.split(".")[0]
+            if base_module in builtin_modules:
+                continue
+            self.visit_module(module, level=0)
+        if hasattr(node, "module") and hasattr(node, "level"):
+            self.visit_module(node.module, node.level)
 
     def visit_ImportFrom(self, node):
+        if not node.module:
+            # can sometimes be None apparently?
+            return
         full_module = self.visit_module(node.module, node.level)
         base_module = full_module.split(".")[0]
-        if base_module in builtin_modules:
+        if base_module != "typing" and base_module in builtin_modules:
             return
         for name in node.names:
-            sym_name = name.name 
+            # TODO: process * imports correctly
+
+            if base_module == "typing" and name.name != "Annotated":
+                # don't bother with non-Annotated stuff
+                continue
+
+            sym_name = name.name
             if self.current_module:
-                sym_name = f"{self.current_module}.{name.name}"
-            full_name = f"{full_module}.{name.name}"
-            self.symbol_import_graph[sym_name] = full_name
+                sym_name = f"{self.current_module}.{sym_name}"
+            import_sym_name = f"{full_module}.{name.name}"
+
+            if import_sym_name in self.annotated_aliases or import_sym_name == "typing.Annotated":
+                self.annotated_aliases.add(sym_name)
+
+            if name.asname:
+                # handle import as case
+                alias_name = name.asname
+                if self.current_module:
+                    alias_name = f"{self.current_module}.{alias_name}"
+
+                if sym_name in self.annotated_aliases:
+                    self.annotated_aliases.add(alias_name)
+
             self.visit(name)
         self.generic_visit(node)
 
     def visit_Assign(self, node):
+        # TODO: handle multiple targets, values
+        if not isinstance(node.targets[0], ast.Name):
+            return
+
         target_name = node.targets[0].id
-        target_full_name = f"{self.current_module}.{target_name}"
-        self.symbol_import_graph[target_full_name] = None
-        if isinstance(node.value, ast.Subscript):
-            if isinstance(node.value.value, ast.Name):
-                value_name = node.value.value.id
-                if value_name == "Annotated":
-                    print(f"Found new annotated alias {target_full_name}")
-                    self.annotated_aliases.add(target_full_name)
-        else:
-            if isinstance(node.value, ast.Name):
-                value_name = node.value.id
-                value_full_name = f"{self.current_module}.{value_name}"
-                value_origin_name = self.get_origin_name(value_full_name)
-                if value_origin_name in self.annotated_aliases:
-                    print(f"Found new annotated alias {target_full_name}")
-                    self.annotated_aliases.add(target_full_name)
-
-                    
-
+        if self.current_module:
+            target_name = f"{self.current_module}.{target_name}"
+        if isinstance(node.value, ast.Name):
+            value_name = node.value.id
+            if self.current_module:
+                value_name = f"{self.current_module}.{value_name}"
+            if value_name in self.annotated_aliases:
+                self.annotated_aliases.add(target_name)
+            else:
+                # was assigned to something else
+                self.annotated_aliases.discard(target_name)
+        elif isinstance(node.value, ast.Subscript) and isinstance(node.value.value, ast.Name):
+            # Check if being assigned to Annotated
+            value_name = node.value.value.id
+            if self.current_module:
+                value_name = f"{self.current_module}.{value_name}"
+            if value_name in self.annotated_aliases:
+                self.annotated_aliases.add(target_name)
+            else:
+                # was assigned to something else
+                self.annotated_aliases.discard(target_name)
+        elif isinstance(node.value, ast.Attribute):
+            value_name = f"{node.value.value.id}{node.value.attr}"
+            if self.current_module:
+                value_name = f"{self.current_module}.{value_name}"
+            if value_name in self.annotated_aliases:
+                self.annotated_aliases.add(target_name)
+            else:
+                # was assigned to something else
+                self.annotated_aliases.discard(target_name)
--- test_static_analysis.py
+++ test_static_analysis.py
@@ -3,7 +3,7 @@ import os
 import importlib
 from pathlib import Path
 from textwrap import dedent
-from static_analysis import AnnotatedAliasDetector, get_path_for_relative_import, get_module_from_path
+from static_analysis import AnnotatedAliasDetector, get_path_for_relative_import, get_module_from_path, get_abs_module
 
 def test_get_path_for_relative_import_module_path():
 
@@ -48,6 +48,14 @@ def test_get_module_from_module_path():
     module_name = get_module_from_path(path, level=3)
     assert module_name == "module_a.foo.bar"
 
+def test_get_abs_module_level_1():
+    abs_module = get_abs_module("numpy.core", "_multiarray_umath", 1)
+    assert abs_module == "numpy.core._multiarray_umath"
+
+def test_get_abs_module_level_2():
+    abs_module = get_abs_module("numpy.core.numeric", "_multiarray_umath", 2)
+    assert abs_module == "numpy.core._multiarray_umath"
+
 def test_basic_import():
     code = dedent("""
     from module_a import FooType
@@ -58,17 +66,14 @@ def test_basic_import():
     analyzer = AnnotatedAliasDetector()
     analyzer.visit(node)
     assert analyzer.annotated_aliases == set(
-        ["module_a.types.FooType", "module_a.types.BarType", "module_a.types.BazType"]
-    )
-    assert (set([
-        ("FooType", "module_a.FooType"),  
-        ("module_a.FooType", "module_a.types.FooType"),  
-        ("module_a.types.FooType", None),  
-        ("module_a.BarType", "module_a.types.BarType"),  
-        ("module_a.types.BarType", None),  
-        ("module_a.BazType", "module_a.types.BazType"),  
-        ("module_a.types.BazType", None),  
-    ]) <= set(analyzer.symbol_import_graph.items())
+        ["FooType", 
+         "module_a.FooType",
+         "module_a.BarType",
+         "module_a.BazType",
+         "module_a.types.FooType",
+         "module_a.types.BarType",
+         "module_a.types.BazType",
+         "module_a.types.Annotated"]
     )
 
 def test_direct_import():
@@ -81,10 +86,139 @@ def test_direct_import():
     analyzer = AnnotatedAliasDetector()
     analyzer.visit(node)
     assert analyzer.annotated_aliases == set(
-        ["module_a.types.FooType", "module_a.types.BarType", "module_a.types.BazType"]
+        ["FooType", "module_a.types.FooType", "module_a.types.BarType", "module_a.types.BazType", "module_a.types.Annotated"]
     )
-    assert (set([
-        ("FooType", "module_a.types.FooType"),  
-        ("module_a.types.FooType", None),  
-    ]) <= set(analyzer.symbol_import_graph.items())
+
+
+def test_jaxtyping_from_import():
+    code = dedent("""
+    from jaxtyping import Float
+    """)
+
+    node = compile(code, filename="<string>", mode="exec", flags=ast.PyCF_ONLY_AST)
+
+    analyzer = AnnotatedAliasDetector()
+    analyzer.visit(node)
+    assert analyzer.annotated_aliases == set(
+        ["Float",
+         "jaxtyping.Float",
+         "jaxtyping.Int8",
+         "jaxtyping.Int32",
+         "jaxtyping.UInt8",
+         "jaxtyping.Int",
+         "jaxtyping.Complex64",
+         "jaxtyping.Float32",
+         "jaxtyping.UInt64",
+         "jaxtyping.Int64",
+         "jaxtyping.Integer",
+         "jaxtyping.BFloat16",
+         "jaxtyping.UInt16",
+         "jaxtyping.Complex",
+         "jaxtyping.Float16",
+         "jaxtyping.Int16",
+         "jaxtyping.UInt32",
+         "jaxtyping.Complex128",
+         "jaxtyping.Float64",
+         "jaxtyping.Key",
+         "jaxtyping.Shaped",
+         "jaxtyping.UInt",
+         "jaxtyping.Inexact",
+         "jaxtyping.Num",
+         "jaxtyping.Float",
+         "jaxtyping.Bool",
+         "jaxtyping._indirection.Int8",
+         "jaxtyping._indirection.Int32",
+         "jaxtyping._indirection.UInt8",
+         "jaxtyping._indirection.Int",
+         "jaxtyping._indirection.Complex64",
+         "jaxtyping._indirection.Float32",
+         "jaxtyping._indirection.UInt64",
+         "jaxtyping._indirection.Int64",
+         "jaxtyping._indirection.Integer",
+         "jaxtyping._indirection.BFloat16",
+         "jaxtyping._indirection.UInt16",
+         "jaxtyping._indirection.Complex",
+         "jaxtyping._indirection.Float16",
+         "jaxtyping._indirection.Int16",
+         "jaxtyping._indirection.UInt32",
+         "jaxtyping._indirection.Complex128",
+         "jaxtyping._indirection.Float64",
+         "jaxtyping._indirection.Key",
+         "jaxtyping._indirection.Shaped",
+         "jaxtyping._indirection.UInt",
+         "jaxtyping._indirection.Inexact",
+         "jaxtyping._indirection.Num",
+         "jaxtyping._indirection.Float",
+         "jaxtyping._indirection.Bool",
+         "jaxtyping._indirection.Annotated",
+         "typeguard._checkers.Annotated"
+        ]
+    )
+
+
+
+def test_jaxtyping_import():
+    code = dedent("""
+    import jaxtyping
+    MyFloat = jaxtyping.Float
+    """)
+
+    node = compile(code, filename="<string>", mode="exec", flags=ast.PyCF_ONLY_AST)
+
+    analyzer = AnnotatedAliasDetector()
+    analyzer.visit(node)
+    assert analyzer.annotated_aliases == set(
+        ["MyFloat",
+         "jaxtyping.Float",
+         "jaxtyping.Int8",
+         "jaxtyping.Int32",
+         "jaxtyping.UInt8",
+         "jaxtyping.Int",
+         "jaxtyping.Complex64",
+         "jaxtyping.Float32",
+         "jaxtyping.UInt64",
+         "jaxtyping.Int64",
+         "jaxtyping.Integer",
+         "jaxtyping.BFloat16",
+         "jaxtyping.UInt16",
+         "jaxtyping.Complex",
+         "jaxtyping.Float16",
+         "jaxtyping.Int16",
+         "jaxtyping.UInt32",
+         "jaxtyping.Complex128",
+         "jaxtyping.Float64",
+         "jaxtyping.Key",
+         "jaxtyping.Shaped",
+         "jaxtyping.UInt",
+         "jaxtyping.Inexact",
+         "jaxtyping.Num",
+         "jaxtyping.Float",
+         "jaxtyping.Bool",
+         "jaxtyping._indirection.Int8",
+         "jaxtyping._indirection.Int32",
+         "jaxtyping._indirection.UInt8",
+         "jaxtyping._indirection.Int",
+         "jaxtyping._indirection.Complex64",
+         "jaxtyping._indirection.Float32",
+         "jaxtyping._indirection.UInt64",
+         "jaxtyping._indirection.Int64",
+         "jaxtyping._indirection.Integer",
+         "jaxtyping._indirection.BFloat16",
+         "jaxtyping._indirection.UInt16",
+         "jaxtyping._indirection.Complex",
+         "jaxtyping._indirection.Float16",
+         "jaxtyping._indirection.Int16",
+         "jaxtyping._indirection.UInt32",
+         "jaxtyping._indirection.Complex128",
+         "jaxtyping._indirection.Float64",
+         "jaxtyping._indirection.Key",
+         "jaxtyping._indirection.Shaped",
+         "jaxtyping._indirection.UInt",
+         "jaxtyping._indirection.Inexact",
+         "jaxtyping._indirection.Num",
+         "jaxtyping._indirection.Float",
+         "jaxtyping._indirection.Bool",
+         "jaxtyping._indirection.Annotated",
+         "typeguard._checkers.Annotated"
+        ]
     )
